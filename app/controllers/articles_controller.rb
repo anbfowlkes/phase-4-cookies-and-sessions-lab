3a0ff6358dab89722149008b8ccdadc85ec31c53
Aaron Fowlkes <afowlkes21@gmail.com>
2022-09-13 21:59:28 -0400

    commit tree a430b93542acc02411e9f43d9631115a7f837a25

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,17 @@ class ArticlesController < ApplicationController
 
   def show
     article = Article.find(params[:id])
-    render json: article
+    # session[:page_views] ||= 1
+    if session[:page_views] == nil
+      session[:page_views] = 1
+      render json: article
+    elsif session[:page_views] < 3
+      session[:page_views] = session[:page_views] + 1
+      render json: article
+    elsif session[:page_views] >= 3
+      session[:page_views] = session[:page_views] + 1
+      render json: { error: "Maximum pageview limit reached" }, status: 401
+    end
   end
 
   private
